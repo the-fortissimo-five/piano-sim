ef4b2612803b54f7aebf9ec705372e026c9d3e1f
Jeff Lawrence <jefflaw13@hotmail.com>
2019-04-09 10:07:13 -0700

    created constructors

--- js/app.js
+++ js/app.js
@@ -11,8 +11,27 @@ var soundArray = [
 ];
 
 var sheetMusicArray = [
-  'odeToJoy', './assets/img/odeDefault.jpg', './assets/img/odePicked.jpg',
+  'odeToJoy', './assets/img/odeDefault.png', './assets/img/odePicked.png',
 ];
 
-console.log(soundArray);
-console.log(sheetMusicArray);
+// console.log(soundArray);
+// console.log(sheetMusicArray);
+var keyboardObject = {};
+var sheetMusicObject = {};
+
+function keyboardObjectConstructor(name, audioURL){
+  this.name = name;
+  this.audioURL = audioURL;
+
+  keyboardObject[this.name] = this;
+}
+// console.log(new keyboardObjectConstructor(soundArray[0], soundArray[1]));
+// console.log(keyboardObject);
+function sheetMusicObjectConstructor(imgFilePath){
+  this.imgFilePath =imgFilePath;
+
+  sheetMusicObject['sheetMusicFilePath'] = this.imgFilePath;
+}
+
+// console.log(new sheetMusicObjectConstructor(sheetMusicArray[1]));
+// console.log(sheetMusicObject);
