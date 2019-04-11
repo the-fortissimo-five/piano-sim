7fce05b4ca3010cc705d7913cf24de36f0d5fd6a
Chacs <felipede_latorre@hotmail.com>
2019-04-10 20:52:39 -0700

    Add tracker but broke sound

--- js/keyboard.js
+++ js/keyboard.js
@@ -1,33 +1,58 @@
 'use strict';
 
+var CHARACTER_DISPLAY_LENGHT = 8;
+var VALID_CHARACTERS = ['S', 'D', 'F', 'J', 'K', 'L', ' ', ';'];
+
 var log = document.getElementById('log');
+
+function limitCharacterlengt(stringOfCharacters) {
+  if (stringOfCharacters.length > CHARACTER_DISPLAY_LENGHT) {
+    stringOfCharacters = stringOfCharacters.slice(1);
+  }
+  return stringOfCharacters;
+}
+
+
+
+function addToTracker(keyPressed){
+  keyPressed = keyPressed.toUpperCase();
+  if (VALID_CHARACTERS.includes(keyPressed)) {
+    var parentId = document.getElementById('trackerContainer');
+    var pElement = document.getElementById('tracker');
+    pElement.textContent += keyPressed;
+    pElement.textContent = limitCharacterlengt(pElement.textContent);
+    parentId.appendChild(pElement);
+  }
+}
+
 
 function keyLogEventHandler(event){
   // console.log(event);
   // console.log(event.code);
   // console.log(keyboardObject);
 
-  log.textContent += ` ${event.code}`;
-  try {
-    keyboardObject[event.code].play();
-  }
-  catch(error){
-    //alert('Lemon Merangue Pie!');
-    console.log('You entered the wrong key');
-  }
+  addToTracker(event.key);
+
+  // log.textContent += ` ${event.code}`;
+  // try {
+  //   keyboardObject[event.code].play();
+  // }
+  // catch(error){
+  //   console.log('You entered the wrong key');
+  // }
 }
 function transitionReset (event){
-  console.log(event);
+  // console.log(event);
   // debugger;
   //console.log('Hello from transition end');
   setTimeout(function(){document.getElementById(keyboardObject[event.code].name).className = 'pianoKeys';
   }, 1000);
-  console.log(keyboardObject[event.code].name);
-  console.log((keyboardObject[event.code].name).className);
+  // console.log(keyboardObject[event.code].name);
+  // console.log((keyboardObject[event.code].name).className);
 }
 function changeTheNameOfThisFunction(event) {
   // event.target.classList.remove('pianoKeys');
-  console.log(keyboardObject[event.code].name);
+  // console.log(keyboardObject[event.code].name);
   document.getElementById(keyboardObject[event.code].name).className = 'played';
   var transition = document.querySelector('.played');
   transition.addEventListener('transitionend',transitionReset(event));
@@ -48,3 +73,5 @@ function clearTextField(){
 function clearHistoryField(){
   log.textContent = '';
 }
+
+
